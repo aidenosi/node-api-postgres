d945f264053f6cb37308c4702f17ad956942e2cb
aidenosi <aidenosi@yahoo.ca>
2019-07-04 13:49:31 -0400

    Small changes to better suit my needs for testing.

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = 3001;
+const db = require("./queries");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get("/", (request, response) => {
   response.json({ info: "Node.js, Express, and Postgres API" });
 });
+
+app.get("/users", db.getUsers);
+app.get("/users/:id", db.getUserById);
+app.get("/users", db.createUser);
+app.get("/users/:id", db.updateUser);
+app.get("/users/:id", db.deleteUser);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
